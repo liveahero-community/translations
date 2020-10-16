8b7e29fb4240420806df0f1a554d2d0039cf60bb
Ze-Hao Wang (Salmon) <salmon.zh.tw@gmail.com>
2020-10-16 21:04:15 +0800

    update readme

--- tools/translate-gen-tsv.ts
+++ tools/translate-gen-tsv.ts
@@ -1,26 +1,28 @@
 // Node modules.
-import fs from 'fs';
 import _ from 'lodash';
-// Local modules.
-import SkillData from '../master-data/latest/ja-JP/SkillMaster.json';
-import StatusData from '../master-data/latest/ja-JP/StatusMaster.json';
+import fs from 'fs-extra';
+import appRoot from 'app-root-path';
+
+const [locale] = process.argv.slice(2);
 
 const main = async () => {
   // File 1.
-  const skills = _.values(SkillData);
+  const skillData = await fs.readJSON(`${appRoot}/master-data/latest/${locale}/SkillMaster.json`);
+  const skills = _.values(skillData);
   const mappedSkills: any[] = _.map(skills, _.partialRight(_.pick, ['skillId', 'skillName', 'description']));
 
   const skillText = mappedSkills.map((data) => `${data.skillId}\t${data.skillName}\t${data.description}`).join('\n');
 
-  fs.writeFileSync('./translations/ja-JP/skills.tsv', skillText, 'utf-8');
+  await fs.writeFile(`${appRoot}/translations/${locale}/skills.tsv`, skillText);
 
   // File 2.
-  const statuses = _.values(StatusData);
+  const statusData = await fs.readJSON(`${appRoot}/master-data/latest/${locale}/StatusMaster.json`);
+  const statuses = _.values(statusData);
   const mappedStatuses: any[] = _.map(statuses, _.partialRight(_.pick, ['statusId', 'statusName', 'description']));
 
   const statusText = mappedStatuses.map((data) => `${data.statusId}\t${data.statusName}\t${data.description}`).join('\n');
 
-  fs.writeFileSync('./translations/ja-JP/statuses.tsv', statusText, 'utf-8');
+  await fs.writeFile(`${appRoot}/translations/${locale}/statuses.tsv`, statusText);
 };
 
 main();
