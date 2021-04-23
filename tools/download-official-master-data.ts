77815623d85393b2661fa5e14e39395633226daf
Ze-Hao Wang <salmon.zh.tw@gmail.com>
2021-04-23 19:53:24 +0800

    Support pre-download

--- tools/download-official-master-data.ts
+++ tools/download-official-master-data.ts
@@ -14,8 +14,8 @@ const getVersion = async (url: string) => {
   const { client, master } = await res.json();
 
   return {
-    appVersion: client,
-    masterVersion: master,
+    appVersion: String(client),
+    masterVersion: parseInt(String(master)),
   };
 };
 
@@ -38,14 +38,16 @@ const download = async (url: string, filedir: string, filename: string, isJson =
   fs.writeFileSync(`${filedir}/${filename}`, text, { encoding: 'utf-8' });
 };
 
-const main = async () => {
-  // Step 1: get app version info.
-  const versionApiUrl = 'https://gateway.live-a-hero.jp/api/status/version';
-  const { appVersion, masterVersion } = await getVersion(versionApiUrl);
-
+const downloadByVersion = async (appVersion?: string, masterVersion?: number) => {
   // Store the version info.
   if (!appVersion || !masterVersion) {
-    console.warn('Cannot get version from server');
+    console.warn('Cannot get version from server, try to pre-download next version.');
+    const currentAppVersion = fs.readFileSync(`${appRoot}/master-data/AppVersion`, 'utf-8');
+    const currentMasterVersion = fs.readFileSync(`${appRoot}/master-data/MasterVersion`, 'utf-8');
+    await downloadByVersion(
+      currentAppVersion,
+      parseInt(currentMasterVersion) + 1,
+    );
     process.exit(0);
   }
 
@@ -67,32 +69,54 @@ const main = async () => {
   const masterDataBaseUrl = `https://d1itvxfdul6wxg.cloudfront.net/datas/master/${masterVersion}`;
   const masterDataCatalogUrl = `${masterDataBaseUrl}/MasterDataCatalog`;
   
-  const masterDataList = await getDataList(masterDataCatalogUrl);
+  const masterDataList: string[] = [];
+  try {
+    masterDataList.push(...await getDataList(masterDataCatalogUrl));
+  } catch {
+    console.log(`Still cannot get next version, process is finishing.`);
+    // Restore original version.
+    fs.writeFileSync(
+      `${appRoot}/master-data/MasterVersion`,
+      String(masterVersion - 1),
+    );
+    process.exit(0);
+  }
 
-  masterDataList.forEach((masterDataItem) => {
+  await Promise.all(masterDataList.map((masterDataItem) => {
     const url = `${masterDataBaseUrl}/${masterDataItem}`;
-    download(
-      url,
-      `${appRoot}/master-data/${masterVersion}/ja-JP`,
-      `${masterDataItem}.json`,
-    );
-    download(
-      url,
-      `${appRoot}/master-data/latest/ja-JP`,
-      `${masterDataItem}.json`,
-    );
     console.log(`Save ${url}`);
-  });
+
+    return Promise.all([
+      download(
+        url,
+        `${appRoot}/master-data/${masterVersion}/ja-JP`,
+        `${masterDataItem}.json`,
+      ),
+      download(
+        url,
+        `${appRoot}/master-data/latest/ja-JP`,
+        `${masterDataItem}.json`,
+      ),
+    ]);
+  }));
 
   // Step 3: get catalog data.
   const catalogDataUrl = `https://d1itvxfdul6wxg.cloudfront.net/datas/catalog/Japanese.properties`;
-  download(
+  await download(
     catalogDataUrl,
     `${appRoot}/master-data/latest/ja-JP`,
     `Japanese.properties`,
     false,
   );
   console.log(`Save ${catalogDataUrl}`);
+};
+
+const main = async () => {
+  // Step 1: get app version info.
+  const versionApiUrl = 'https://gateway.live-a-hero.jp/api/status/version';
+  const { appVersion, masterVersion } = await getVersion(versionApiUrl);
+
+  downloadByVersion(appVersion, masterVersion);
 };
 
 main();
